d359a6d92ff27272a07eef39dc8436af3307743e
Kevin Harrington <mad.hephaestus@gmail.com>
2017-10-08 17:49:44 -0400

    loading parts

--- PlaceParts.groovy
+++ PlaceParts.groovy
@@ -6,14 +6,14 @@ return new ICadGenerator(){
 		DHLink dh = dhLinks.get(linkIndex)
 		// Transform used by the UI to render the location of the object
 		Affine manipulator = dh.getListener();
-		CSG legPart  = new Cube(40).toCSG()
+		CSG legPart  = new Cube(20).toCSG()
 		legPart.setColor(javafx.scene.paint.Color.WHITE);
 		return [legPart].collect{
 			it.setManipulator(manipulator)
 		}
 	}
 	public ArrayList<CSG> generateBody(MobileBase b ) {
-		CSG body  = new Cube(40).toCSG()
+		CSG body  = new Cube(20).toCSG()
 		body.setColor(javafx.scene.paint.Color.BLUE);	
 		return [body].collect{
 			it.setManipulator(b.getRootListener());
